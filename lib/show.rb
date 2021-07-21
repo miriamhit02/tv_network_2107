179b8420247c3380f94d5d788aa6c84f44e529f0
Miriam Aceves <miriammerari02@gmail.com>
2021-07-21 12:55:46 -0400

    Create shoe class and all required methods

--- lib/show.rb
+++ lib/show.rb
@@ -1,1 +1,30 @@
+require 'pry'
+class Show
+  attr_reader :name, :creator, :characters
 
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+  end
+
+  def total_salary
+    @characters.sum do |character|
+      character.salary
+    end
+  end
+
+  def highest_paid_actor
+
+    highest_paid = @characters.max_by do |character|
+      character.salary
+    end
+    highest_paid.actor
+  end
+
+  def actors
+    @characters.map do |character|
+      character.actor
+    end
+  end
+end
